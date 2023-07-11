1a572a4a2b390b7c8985645e8a6fdd7da47508be
David M. Colvin <davidmcolvin@gmail.com>
2023-07-10 22:14:42 -0400

    Create a connection to sqlServer and create a person with results.

--- DataAccessSqlDemo/FormUI/DataAccess.cs
+++ DataAccessSqlDemo/FormUI/DataAccess.cs
@@ -3,14 +3,57 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace FormUI
 {
   public class DataAccess
   {
-    public List<Person> GetPersons(string lastName)
+    string dbName = "Sample";
+
+    public List<Person> GetPersons(string lastNameSearchString)
     {
-      throw new NotImplementedException();
+      List<Person> output = new List<Person>();
+
+      using (SqlConnection connection = new SqlConnection(Helper.CnnVal(dbName)))
+      {
+        using (SqlCommand command = new SqlCommand("Persons_GetByLastName", connection))
+        {
+          // Set the command type to stored procedure
+          command.CommandType = CommandType.StoredProcedure;
+
+          // Add the parameter to the command
+          command.Parameters.AddWithValue("@p_LastName", lastNameSearchString);
+
+
+          connection.Open();
+
+          // Execute the stored procedure
+          using (SqlDataReader reader = command.ExecuteReader())
+          {
+            // Process the results
+            while (reader.Read())
+            {
+              // Retrieve column values using column names
+              string lastName = reader.GetString(reader.GetOrdinal("LastName"));
+              string firstName = reader.GetString(reader.GetOrdinal("FirstName"));
+              string emailAddress = reader.GetString(reader.GetOrdinal("EmailAddress"));
+
+              Person person = new Person {
+                LastName = lastName,
+                FirstName = firstName,
+                Email = emailAddress
+                };
+
+              // Do something with the Person object
+              Console.WriteLine($"Name: {person.FirstName} {person.LastName}, Email: {person.Email}");
+            }
+          }
+        }
+      }
+      return output;
+
     }
   }
 }
